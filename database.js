28a1be450f047b29b695d89bea0e7138b4db85ad
8Krystof8 <132814628+8Krystof8@users.noreply.github.com>
2025-11-24 12:13:05 +0100

    Add global database of questions and answers

---
+++ database.js
@@ -0,0 +1,310 @@
+window.globalDatabase = [
+  {
+    "question": "When a new motherboard is being installed, between which two components must thermal compound be applied?",
+    "correct_answers": [
+      "CPU",
+      "heat sink"
+    ]
+  },
+  {
+    "question": "A technician is explaining the differences between inkjet printers and laser printers to a customer. Which two comparisons should the technician make?",
+    "correct_answers": [
+      "Inkjet printers are less expensive as an initial purchase than laser printers.",
+      "The laser printer output is dry after printing, but an inkjet printer may still have wet ink on the paper after printing."
+    ]
+  },
+  {
+    "question": "Which three components are typically found in laser printer maintenance kits?",
+    "correct_answers": [
+      "fuser assembly",
+      "pickup rollers",
+      "transfer rollers"
+    ]
+  },
+  {
+    "question": "Which three features can be configured in the BIOS settings to secure a computer?",
+    "correct_answers": [
+      "drive encryption",
+      "TPM",
+      "passwords"
+    ]
+  },
+  {
+    "question": "What are two safety hazards when dealing with laser printers?",
+    "correct_answers": [
+      "high voltage",
+      "hot components"
+    ]
+  },
+  {
+    "question": "What three values must be added to the IPv4 properties of a NIC in order for a computer to have basic connectivity with the network?",
+    "correct_answers": [
+      "subnet mask",
+      "default gateway",
+      "IP address"
+    ]
+  },
+  {
+    "question": "After a technician tests a theory of probable causes, what action should the technician take if the testing did not identify an exact cause?",
+    "correct_answers": [
+      "Establish a new theory of probable causes."
+    ]
+  },
+  {
+    "question": "What two motherboard components control the system boot operations?",
+    "correct_answers": [
+      "BIOS chip",
+      "UEFI chip"
+    ]
+  },
+  {
+    "question": "What are two types of wired high-speed Internet connections?",
+    "correct_answers": [
+      "cable",
+      "DSL"
+    ]
+  },
+  {
+    "question": "In what two situations would UDP be better than TCP as the preferred transport protocol?",
+    "correct_answers": [
+      "when a faster delivery mechanism is needed",
+      "when delivery overhead is not an issue"
+    ]
+  },
+  {
+    "question": "Which two laptop components are considered replaceable by the customer?",
+    "correct_answers": [
+      "battery",
+      "RAM"
+    ]
+  },
+  {
+    "question": "A user wants to synchronize some apps, movies, music, and ebooks between mobile devices. What are the two types of connections used to synchronize this type of data?",
+    "correct_answers": [
+      "Wi-Fi",
+      "USB"
+    ]
+  },
+  {
+    "question": "A computer technician is installing a RAID. If the RAID uses striping with parity, which RAID level is the technician using?",
+    "correct_answers": [
+      "5"
+    ]
+  },
+  {
+    "question": "A computer technician is installing a RAID. If the RAID uses mirroring and striping, which RAID level is the technician using?",
+    "correct_answers": [
+      "10"
+    ]
+  },
+  {
+    "question": "A computer technician is installing a RAID. If the RAID uses striping with double parity, which RAID level is the technician using?",
+    "correct_answers": [
+      "6"
+    ]
+  },
+  {
+    "question": "A computer technician is installing a RAID. If the RAID uses mirroring, which RAID level is the technician using?",
+    "correct_answers": [
+      "1"
+    ]
+  },
+  {
+    "question": "A computer technician is installing a RAID. If the RAID uses striping (without parity or mirroring), which RAID level is the technician using?",
+    "correct_answers": [
+      "0"
+    ]
+  },
+  {
+    "question": "Which security technique should be recommended for drive redundancy?",
+    "correct_answers": [
+      "Implement a RAID."
+    ]
+  },
+  {
+    "question": "Which security technique should be recommended for phishing?",
+    "correct_answers": [
+      "Provide security awareness training."
+    ]
+  },
+  {
+    "question": "Which security technique should be recommended for protecting a computer from someone using a bootable disk containing hacking tools?",
+    "correct_answers": [
+      "Disable ports."
+    ]
+  },
+  {
+    "question": "Which security technique should be recommended for a manager traveling who needs access to internal corporate resources?",
+    "correct_answers": [
+      "Use a VPN."
+    ]
+  },
+  {
+    "question": "Which security technique should be recommended for a small company that wants stateful firewall services?",
+    "correct_answers": [
+      "Buy an ASA."
+    ]
+  },
+  {
+    "question": "Which security technique should be recommended for a company that wants a device to monitor incoming and outgoing traffic and stop any malicious activity?",
+    "correct_answers": [
+      "Buy an IPS."
+    ]
+  },
+  {
+    "question": "Which security technique should be recommended for a machine where the antimalware software cannot remove all of the malware?",
+    "correct_answers": [
+      "Use Windows Safe Mode."
+    ]
+  },
+  {
+    "question": "Which term describes a file system that supports a partition size up to 16 exabytes?",
+    "correct_answers": [
+      "NTFS"
+    ]
+  },
+  {
+    "question": "Which term describes a partition that is used to store and boot an operating system by default?",
+    "correct_answers": [
+      "active partition"
+    ]
+  },
+  {
+    "question": "Which term describes a file system for USB flash drives that supports files larger than 4 GB?",
+    "correct_answers": [
+      "exFAT"
+    ]
+  },
+  {
+    "question": "Which term describes a partition that supports up to 23 logical drives?",
+    "correct_answers": [
+      "extended partition"
+    ]
+  },
+  {
+    "question": "Which term describes a type of partition that would only exist on a hard disk that has an MBR?",
+    "correct_answers": [
+      "logical drive"
+    ]
+  },
+  {
+    "question": "Which term describes a file system for optical disk media?",
+    "correct_answers": [
+      "CDFS"
+    ]
+  },
+  {
+    "question": "Users in a company have complained about network performance. After investigation, the IT staff has determined that the attacker is using a vulnerability that is known to the software vendor, but not patched yet. What is the type of network attack?",
+    "correct_answers": [
+      "zero-day"
+    ]
+  },
+  {
+    "question": "Users in a company have complained about network performance. After investigation, the IT staff has determined that zombies were used to attack the firewall. What is the type of network attack?",
+    "correct_answers": [
+      "DDoS"
+    ]
+  },
+  {
+    "question": "Users in a company have complained about network performance. After investigation, the IT staff has determined that the attacker is using a table of words that potentially could be used as passwords. What is the type of network attack?",
+    "correct_answers": [
+      "dictionary"
+    ]
+  },
+  {
+    "question": "Users in a company have complained about network performance. After investigation, the IT staff has determined that the attacker injected false records on the server that translates IP addresses to domain names. What is the type of network attack?",
+    "correct_answers": [
+      "DNS poisoning"
+    ]
+  },
+  {
+    "question": "Users in a company have complained about network performance. After investigation, the IT staff has determined that the attacker is using a technique that compares hashed passwords to potential hashes the hacker has. What is the type of network attack?",
+    "correct_answers": [
+      "rainbow table"
+    ]
+  },
+  {
+    "question": "Users in a company have complained about network performance. After investigation, the IT staff has determined that the attacker was using a specific technique that affected the TCP three-way handshake. What is the type of network attack?",
+    "correct_answers": [
+      "SYN flood"
+    ]
+  },
+  {
+    "question": "A technician is working on a laptop and an error message about decryption appears when opening an email. What is a possible solution?",
+    "correct_answers": [
+      "Install an appropriate certificate."
+    ]
+  },
+  {
+    "question": "A technician is working on a laptop and a key on the keyboard is not responding as it should. What is a possible solution?",
+    "correct_answers": [
+      "Clean with compressed air."
+    ]
+  },
+  {
+    "question": "A technician is working on a laptop and GPS is not functioning. What is a possible solution?",
+    "correct_answers": [
+      "Verify that airplane mode is not activated."
+    ]
+  },
+  {
+    "question": "Why would a manager need to use the tracert command?",
+    "correct_answers": [
+      "to display a list of the near-side router interfaces between the source device and the destination device"
+    ]
+  },
+  {
+    "question": "Why would a manager need to use the sfc command?",
+    "correct_answers": [
+      "to scan and verify the integrity of all protected system files and replace incorrect versions with correct versions"
+    ]
+  },
+  {
+    "question": "Why would a technician need to use the DISM command?",
+    "correct_answers": [
+      "to repair, prepare, and fix Windows images and the recovery image within an installation of the operating system"
+    ]
+  },
+  {
+    "question": "Why would a technician need to use the tasklist command?",
+    "correct_answers": [
+      "to display a list of current processes running on a local or a remote computer"
+    ]
+  },
+  {
+    "question": "Why would an analyst need to use the taskkill command?",
+    "correct_answers": [
+      "to end one or more processes on a PC based on process ID or image name"
+    ]
+  },
+  {
+    "question": "Why would an analyst need to use the msconfig command?",
+    "correct_answers": [
+      "to select some programs that will automatically run at startup"
+    ]
+  },
+  {
+    "question": "Why would an analyst need to use the dir command?",
+    "correct_answers": [
+      "to show all of the files in the current directory"
+    ]
+  },
+  {
+    "question": "Why would a bank manager need to use the gpupdate command?",
+    "correct_answers": [
+      "to make the PC synchronize with new group policy settings"
+    ]
+  },
+  {
+    "question": "Why would a bank manager need to use the gpresult command?",
+    "correct_answers": [
+      "to verify the settings that are suspected to be incorrect"
+    ]
+  },
+  {
+    "question": "Why would a lawyer need to use the cd command?",
+    "correct_answers": [
+      "to change from the current working folder to another folder"
+    ]
+  }
+];
